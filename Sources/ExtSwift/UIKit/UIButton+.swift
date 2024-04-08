f68a7c9aa492703133f054388bfb6aa3f1df6e0f
Míng <minglq.9@gmail.com>
2024-04-09 04:27:37 +0800

    fea: UIButton + possible states - Swift 简化实现

--- Sources/ExtSwift/UIKit/UIButton+.swift
+++ Sources/ExtSwift/UIKit/UIButton+.swift
@@ -17,30 +17,12 @@ extension UIButton {
 extension UIButton {
     
     private func with(_ state: State, possible states: [State], closure: (_ state: State) -> Void) {
-        /// state:    0b xxxx 0100 // 任意个 1，一般只有 1 个
-        /// all:      0b 0... 1111 // 后半段全是 1
-        /// states:   0b 0... 1010 // 有若干个 1
-        /// optional: 0b 0... 0001 // 只有 1 个 1
-        /// 基于上面取值：
-        /// 除 state 本身的 0100 之外，
-        /// 当 optional 为 0010、1000 时（与 states 存在并集），
-        /// 将 state 与 optional 取合计得到 0110、1100，
-        /// 排除与 state 相等的值（这里没有），
-        /// 回调
-        closure(state) /// 对 state 回调
-        let all = ~(State.application.rawValue | State.reserved.rawValue)
-        var states: UInt = states.reduce(0) { $0 | $1.rawValue },
-            optional: UInt = 1
-        while states & all != 0 { /// 只要 states 中还有 1
-            if states & optional != 0 { /// 如果 states 和 optional 有相同位置的 1
-                let merged = State(rawValue: state.rawValue | optional) /// 合并 state 和 optional
-                if merged != state { /// 避免对 state 状态回调，导致重复设置
-                    closure(merged) /// 对 states.map { state | $0 } 回调
-                }
-                states &= ~optional /// 从 states 中去掉 optional 中的 1
+        closure(state)
+        for each in states {
+            let merged = State(rawValue: state.rawValue | each.rawValue)
+            if merged != state {
+                closure(merged)
             }
-            /// optional 中的 1 从最右端开始左移，直到清理掉 states 中所有的 1
-            optional <<= 1
         }
     }
     
