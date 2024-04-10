d3a5c4bb9ae5add431287836c0d7a771ae753ca2
Míng <minglq.9@gmail.com>
2024-04-10 19:11:11 +0800

    opt: ESHitTestView convenience init

--- Sources/ExtSwift/UIKit/ESHitTestView.swift
+++ Sources/ExtSwift/UIKit/ESHitTestView.swift
@@ -12,11 +12,10 @@ open class ESHitTestView: UIView {
     
     open var hitTestClosure: ((UIView?, CGPoint, UIEvent?) -> UIView?)?
     
-    public init(hitTestClosure: ((UIView?, CGPoint, UIEvent?) -> UIView?)? = nil) {
+    public convenience init(hitTestClosure: ((UIView?, CGPoint, UIEvent?) -> UIView?)? = nil) {
+        self.init(frame: .zero)
         self.hitTestClosure = hitTestClosure
-        super.init(frame: .zero)
     }
-    public required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
     open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         let hitView = super.hitTest(point, with: event)
